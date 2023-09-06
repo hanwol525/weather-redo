f97714053070d93ade0b5d7b87d98fe26da2c9bb
HanWol <hannahwolfson05@gmail.com>
2023-09-06 12:15:40 -0500

    morning updates

--- weather/src/components/pages/forecast/Daily.js
+++ weather/src/components/pages/forecast/Daily.js
@@ -1,33 +1,30 @@
 import React from "react";
 
 export default function Daily(props) {
-    let dailyWeather
-    let dailyWind
-    let dailyHum
-
-    const coordsSearch = (lat, lon) => {
-        let fiveDayCoords = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=23116274a5a42433f230b2d7ad947f9a&units=imperial';
-        let dailyCoords = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=23116274a5a42433f230b2d7ad947f9a&units=imperial';
-        fetch (dailyCoords).then((res) => {
-            return res.json()
-        }).then((data) => {
-            dailyWeather = data.main.temp;
-            dailyWind = data.wind.speed;
-            dailyHum = data.main.humidity;
-        });
-
-        return dailyWeather, dailyWind, dailyHum
-    };
+    
 
     // gotta find the way to set this shit up for the daily and 5 day
     // thoughts: get the geocoding fxn in here, also get some way to see if like...
     // the info can transfer in and i can use it in the fuckin return statement??
+    // HALF BAKED THOUGHT: basically putting the fetch in the main forecast page and using
+    // THAT as a way to get the info and slot it into the components; can use props on this component
 
     return(
-        <div>
-            <p>{dailyWeather}</p>
-            <p>{dailyWind}</p>
-            <p>{dailyHum}</p>
+        <div className="container">
+            <div className="card col-lg-3">
+                <div className="card-body d-flex row justify-content-center">
+                    {/* <img className="card-img-top" 
+                        src={props.src}
+                        alt="Project preview"
+                        style={styles.previewImg}
+                        ></img> */}
+                    <h4 className="card-title text-center my-3">Hey there</h4>
+                    <div className="d-flex row justify-content-around my-2">
+                        {/* `<a href={props.github} className="btn btn-primary col-5">Repository</a>
+                        <a href={props.deployed} className="btn btn-primary col-5">Deployment/Preview</a>` */}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- weather/src/components/pages/forecast/Forecast.js
+++ weather/src/components/pages/forecast/Forecast.js
@@ -3,7 +3,25 @@ import Search from "./SearchBar";
 
 export default function Forecast(){
 
-    return(
+    let dailyWeather
+    let dailyWind
+    let dailyHum
+
+    const dailyCoordsSearch = (lat, lon) => {
+        let fiveDayCoords = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=23116274a5a42433f230b2d7ad947f9a&units=imperial';
+        let dailyCoords = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=23116274a5a42433f230b2d7ad947f9a&units=imperial';
+        fetch (dailyCoords).then((res) => {
+            return res.json()
+        }).then((data) => {
+            dailyWeather = data.main.temp;
+            dailyWind = data.wind.speed;
+            dailyHum = data.main.humidity;
+        });
+
+        return dailyWeather, dailyWind, dailyHum
+    };
+
+    return (
         <div className="sec vh-100">
         </div>
     )
